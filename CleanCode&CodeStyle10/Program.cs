2d423f13b3e6c400b7eb6ac7069dab07c39a0b82
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-10-05 18:22:01 +0500

    Добавьте файлы проекта.

---
+++ CleanCode&CodeStyle10/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CleanCode_CodeStyle10
+{
+    internal class Program
+    {
+        public void Enable()
+        {
+            _enable = true;
+
+            _effects.StartEnableAnimation();
+        }
+
+        public void Disable()
+        {
+            _enable = false;
+
+            _pool.Free(this);
+        }
+    }
+}
